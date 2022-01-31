fef401f59ab5b4aee26858927ebd66e18309d4d9
MisuruAraki <araki@ceed.jp>
2022-02-01 07:43:50 +0900

    走行回数表示を追加

--- app/src/main/java/jp/ceed/android/mylapslogger/dao/PracticeTrackDao.kt
+++ app/src/main/java/jp/ceed/android/mylapslogger/dao/PracticeTrackDao.kt
@@ -9,7 +9,8 @@ import jp.ceed.android.mylapslogger.entity.TotalDistance
 interface PracticeTrackDao {
 
     @Query(
-        "SELECT p.id, p.track_id, t.name, p.lap_count, p.best_lap, p.start_time, p.end_time, p.display_time, p.total_training_time " +
+        "SELECT p.id, p.track_id, t.name, p.lap_count, p.best_lap, " +
+                "p.start_time, p.end_time, p.display_time, p.total_training_time " +
                 "FROM Practice p, Track t WHERE p.track_id = t.id " +
                 "AND p.track_id = (:trackId) " +
                 "ORDER BY p.best_lap limit 1 "
@@ -17,10 +18,11 @@ interface PracticeTrackDao {
     fun findBestLapByTrackId(trackId: Int): PracticeTrack?
 
     @Query(
-        "select t.id, t.name, t.length, sum(p.lap_count) as total_lap_count, (t.length * sum(p.lap_count)) as distance " +
-                "from Practice p, Track t " +
-                "where p.track_id = t.id " +
-                "group by track_id"
+        "SELECT t.id, t.name, t.length, sum(p.lap_count) AS total_lap_count, " +
+                "(t.length * sum(p.lap_count)) AS distance, count(p.id) AS training_count " +
+                "FROM Practice p, Track t " +
+                "WHERE p.track_id = t.id " +
+                "GROUP BY track_id"
 
     )
     fun getTotalDistance(): List<TotalDistance>
--- app/src/main/java/jp/ceed/android/mylapslogger/entity/TotalDistance.kt
+++ app/src/main/java/jp/ceed/android/mylapslogger/entity/TotalDistance.kt
@@ -9,7 +9,8 @@ data class TotalDistance(
     val name: String,
     val length: Int,
     @ColumnInfo(name = "total_lap_count") val totalLapCount: Int,
-    val distance: Int
+    val distance: Int,
+    @ColumnInfo(name = "training_count") val trainingCount: Int
 ){
     fun displayDistance(): String{
         val fDistance: Float = distance / 1000F
