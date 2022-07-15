6406ce4d838d934cce3c69733357ecc6e21e19ec
MisuruAraki <araki@ceed.jp>
2022-07-15 17:48:04 +0900

    ActivitiesFragmentViewModelTestの追加

--- app/src/main/java/jp/ceed/android/mylapslogger/viewModel/ActivitiesFragmentViewModel.kt
+++ app/src/main/java/jp/ceed/android/mylapslogger/viewModel/ActivitiesFragmentViewModel.kt
@@ -5,15 +5,12 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import jp.ceed.android.mylapslogger.entity.Event
-import jp.ceed.android.mylapslogger.entity.EventState
 import jp.ceed.android.mylapslogger.model.ActivitiesItem
 import jp.ceed.android.mylapslogger.repository.ApiRepository
 import jp.ceed.android.mylapslogger.repository.UserAccountRepository
 import jp.ceed.android.mylapslogger.util.ExceptionUtil
-import kotlinx.coroutines.DelicateCoroutinesApi
 import javax.inject.Inject
 
-@DelicateCoroutinesApi
 @HiltViewModel
 class ActivitiesFragmentViewModel @Inject constructor (
     var apiRepository: ApiRepository,
@@ -34,7 +31,7 @@ class ActivitiesFragmentViewModel @Inject constructor (
     }
 
     fun checkAccount() {
-        val token: String? = userAccountRepository.getAccessToken();
+        val token: String? = userAccountRepository.getAccessToken()
         val userId: String? = userAccountRepository.getUserId()
         if (token == null || userId == null) {
             event.value = Event(EventState.GO_TO_LOGIN)
