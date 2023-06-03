ab4ea8f75637c00062732dc30e3c67d9c109285e
MisuruAraki <araki@ceed.jp>
2023-06-03 10:08:44 +0900

    総走行距離を追加

--- app/src/main/java/jp/ceed/android/mylapslogger/viewModel/TotalDistanceFragmentViewModel.kt
+++ app/src/main/java/jp/ceed/android/mylapslogger/viewModel/TotalDistanceFragmentViewModel.kt
@@ -4,21 +4,26 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import jp.ceed.android.mylapslogger.R
 import jp.ceed.android.mylapslogger.entity.TotalDistance
 import jp.ceed.android.mylapslogger.repository.PracticeTrackRepository
+import jp.ceed.android.mylapslogger.repository.ResourceRepository
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class TotalDistanceFragmentViewModel @Inject constructor(
     var practiceTrackRepository: PracticeTrackRepository,
+    var resourceRepository: ResourceRepository,
 ): ViewModel() {
 
     val totalDistanceList: MutableLiveData<List<TotalDistance>> = MutableLiveData()
 
     init{
         viewModelScope.launch {
-            totalDistanceList.value = practiceTrackRepository.getTotalDistanceList()
+            totalDistanceList.value = practiceTrackRepository.getTotalDistanceList(
+                resourceRepository.getString(R.string.label_total_distance_all)
+            )
         }
     }
 
