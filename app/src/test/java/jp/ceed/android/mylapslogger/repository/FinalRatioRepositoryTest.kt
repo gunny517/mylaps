bf61cdb8c400566f48ff57c05641d1d04770641d
MisuruAraki <araki@ceed.jp>
2024-04-28 06:08:40 +0900

    junit4からjunit5への変更。Spekの削除。

---
+++ app/src/test/java/jp/ceed/android/mylapslogger/repository/FinalRatioRepositoryTest.kt
@@ -0,0 +1,35 @@
+package jp.ceed.android.mylapslogger.repository
+
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+class FinalRatioRepositoryTest {
+
+    @BeforeEach
+    fun setUp() {
+
+    }
+
+    @AfterEach
+    fun tearDown() {
+    }
+
+    @Test
+    fun getFinalRatioData() {
+        assertEquals("1", "1")
+    }
+
+    @Test
+    fun getSavedValue() {
+    }
+
+    @Test
+    fun getContext() {
+    }
+
+    @Test
+    fun setContext() {
+    }
+}
